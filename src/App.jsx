fb9ac9068efcb7403c402264aaa6106b59f7adf4
Adrian <reyes3190@gmail.com>
2025-10-26 20:57:13 +0100

    settings react router

--- src/App.jsx
+++ src/App.jsx
@@ -12,29 +12,32 @@ import ServicesLanding from './pages/ServicesLanding';
 import ScrollToTop from './components/ScrollToTop';
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
+import { HashRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-      <ScrollToTop />
-      <div className="min-h-screen flex flex-col">
-        <Header />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="registro" element={<RegisterPage />} />
-            <Route path="login" element={<LoginPage />} />
-            <Route path="/" element={<Home />} />
-            <Route path="/servicios" element={<ServicesLanding />} />
-            <Route path="/servicio/:id" element={<ServiceDetail />} />
-            <Route path="/perfil" element={<Profile />} />
-            <Route path="/guardados" element={<Favorites />} />
-            <Route path="/publicar" element={<CreateAd />} />
-            <Route path="/editar-servicio/:id" element={<CreateAd />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <HashRouter>
+      <Router>
+        <ScrollToTop />
+        <div className="min-h-screen flex flex-col">
+          <Header />
+          <main className="flex-grow">
+            <Routes>
+              <Route path="registro" element={<RegisterPage />} />
+              <Route path="login" element={<LoginPage />} />
+              <Route path="/" element={<Home />} />
+              <Route path="/servicios" element={<ServicesLanding />} />
+              <Route path="/servicio/:id" element={<ServiceDetail />} />
+              <Route path="/perfil" element={<Profile />} />
+              <Route path="/guardados" element={<Favorites />} />
+              <Route path="/publicar" element={<CreateAd />} />
+              <Route path="/editar-servicio/:id" element={<CreateAd />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </HashRouter>
   );
 }
 
